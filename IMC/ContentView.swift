b71ac62209d2eed248ced0c9e7ba7671a8965199
AIWASS23 <marcelodearaujo677@gmail.com>
2024-06-11 10:09:57 -0300

    Initial Commit

---
+++ IMC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IMC
+//
+//  Created by marcelodearaujo on 11/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by marcelodearaujo on 11/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
